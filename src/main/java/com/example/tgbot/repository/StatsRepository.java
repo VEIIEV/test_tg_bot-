78d5e43c99aab605a7f8bb312185ea8479c06283
veiiev <worldloflwarcraft@mail.ru>
2022-09-19 02:34:41 +0400

    добавил возможность получать информацию о поступлениях больше указанной суммы, с помощью jdbc темплейта, но пока не зарегестрировал команду в боте

--- src/main/java/com/example/tgbot/repository/StatsRepository.java
+++ src/main/java/com/example/tgbot/repository/StatsRepository.java
@@ -16,4 +16,23 @@ public class StatsRepository {
     public int getCountOfIncomesThatGreaterThan(BigDecimal amount){
         return  jdbcTemplate.queryForObject("SELECT count(*) FROM incomes WHERE income > ? ", Integer.class, amount );
     }
+
+
+    //способ реализации того же самого только через именнованые темплейты
+    /*private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public int getCountOfIncomesThatGreaterThan(BigDecimal amount){
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("amount", amount);
+        return  namedParameterJdbcTemplate.queryForObject("SELECT count(*) FROM incomes WHERE income > :amount ", parameters, new StatsRowMapper());
+
+    }
+
+    private static final class StatsRowMapper implements RowMapper<Integer> {
+        @Override
+        public Integer mapRow(ResultSet resultSet, int i) throws SQLException {
+            return resultSet.getInt("COUNT");
+        }
+    }
+    */
 }
